cb28884d2e10be1510fe3afe7b223d3d0ae0a01c
maxineparedes <maxineparedes@gmail.com>
2025-03-27 18:48:33 -0700

    added description to get_summary function

--- R/summary_stats.R
+++ R/summary_stats.R
@@ -2,6 +2,7 @@
 library(tidymodels)
 
 get_summary <- function(dataset) {
+    # generates table containing summary stats for numerical data
   dataset |>
     select(age, shell_weight, diameter, height) |>
     pivot_longer(cols = height:age, names_to = "variable", values_to = "values") |>
