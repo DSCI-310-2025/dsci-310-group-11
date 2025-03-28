17309a73fea17983d3a95e5a7a88ae56c25c4413
maxineparedes <maxineparedes@gmail.com>
2025-03-27 18:40:01 -0700

    create summary stats function

---
+++ R/summary_stats.R
@@ -0,0 +1,17 @@
+
+library(tidymodels)
+library(dplyr)
+
+get_summary <- function(dataset) {
+  dataset |>
+    select(age, shell_weight, diameter, height) |>
+    pivot_longer(cols = height:age, names_to = "variable", values_to = "values") |>
+    group_by(variable) |>
+    summarize(
+      mean = mean(values, na.rm = TRUE),
+      median = median(values, na.rm = TRUE),
+      variance = var(values, na.rm = TRUE),
+      minimum = min(values, na.rm = TRUE),
+      maximum = max(values, na.rm = TRUE)
+    )
+}
