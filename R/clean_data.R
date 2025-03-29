29529e0b58a8dbc050f3e41bcc0a13fed7de8db3
emmaw20 <emmaw20@student.ubc.ca>
2025-03-29 12:25:31 -0700

    added specification for second function

--- R/clean_data.R
+++ R/clean_data.R
@@ -2,18 +2,26 @@
 #' creating a new age target variable, 
 #' removing the old target variable 
 #' and removing an unnecessary categorical variable. 
-#'
-#' Then creates the training and testing sets. 
+ 
 #' @param data_frame Abalone dataframe 
 #' 
 #' @return a data_frame with reversed scaling, new target variable, removed columns 
 #' 
-#'@export 
+#' @export 
+#'
+#' @examples 
+#' clean_data(abalone_data)
 
 clean_data <- function(raw_data = "abalone data to be cleaned") {
 
     #returns clean data according to the above specifications
 }
+
+
+#' Creates the training and testing sets.
+#' @param data_frame clean data that is ready to be split into traininng and testing
+#' 
+#' @return assigns training and testing sets to variables 
 
 split_data <- function(clean_data = "clean abalone data") {
     
